9a2c70c0155b67ab3e1f529f1fdbb8fe72cbd3dc
jessicalima22 <jessica.lima22@hotmail.com>
2023-07-31 23:55:40 -0300

    timer settings

--- script.js
+++ script.js
@@ -197,10 +197,28 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
-//Fake always logged in
-currentAccount = account1;
-updateUI(currentAccount);
-containerApp.style.opacity = 100;
+//Logout timer
+const startLogOutTimer = function () {
+  //set time to 5 minutes
+  let time = 10;
+  //call the timer every second
+  const timer = setInterval(function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+    //in each call, print  the remaining time to UI
+
+    labelTimer.textContent = `${min}:${sec}`;
+    //decrease 1 sec
+    time--;
+
+    //when 0 seconds, stop timer and logout
+    if (time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = `Log in to get started`;
+      containerApp.style.opacity = 0;
+    }
+  }, 1000);
+};
 
 //login settings
 btnLogin.addEventListener('click', function (e) {
@@ -250,6 +268,7 @@ btnLogin.addEventListener('click', function (e) {
     //clear the input fields
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
+    startLogOutTimer();
     updateUI(currentAccount);
   }
 });
